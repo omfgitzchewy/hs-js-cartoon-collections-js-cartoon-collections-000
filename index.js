5c5efe8cc7bb7033102434fd318fe9c2b7f82af1
Aaron Chu <aaronchu357@gmail.com>
2019-03-13 18:44:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
   var cheese = ["cheddar", "gouda", "camembert"]
-  for (var i = foods.length; i >= 0; i--){
+  for (var i = foods.length -1 ; i >= 0; i--){
     if (foods[i] == cheese){
       return foods[i]
     }
