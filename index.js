29d0b2fe268edc6096feec118245f1fdcb6d549a
Aaron Chu <aaronchu357@gmail.com>
2019-03-13 19:10:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+  var str = ""
+  for (var i = 0; i < dwarves.length; i++){
+    str += (i+1 + ". " + dwarves[i] + " ")
+  }
+  return str
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = []
+  for (var i = 0; i < planeteerCalls.length; i++){
+    var changed = planeteerCalls[i].toUpperCase() + "!"
+    arr.push(changed)
+  }
+  return arr
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camembert"]
+  for (var i = 0; i < foods.length; i++){
+    if (cheese.indexOf(foods[i]) !== -1){
+     return foods[i]
+    }
+   }
+// var found = foods.find(function(element) {
+  // return element == cheese;
+// });
+  return "no cheese!"
 }
