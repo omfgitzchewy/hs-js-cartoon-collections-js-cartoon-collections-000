7b74083d726e7a9146ed0fa2bd10fdbae19804b7
Aaron Chu <aaronchu357@gmail.com>
2019-03-13 18:16:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function summonCaptainPlanet(planeteerCalls){
 function longPlaneteerCalls(words) {
   for(var i = 0; i < words.length; i++){
     if (words[i].length <= 4){
-      return true;
+      return true
     } else (words[i].length > 4){
-      return false;
+      return false
     }
   }
 }
