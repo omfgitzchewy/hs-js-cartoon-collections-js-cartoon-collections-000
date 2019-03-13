548bb5819c5524c75789ab6f9f3bb0cabbcdaa9c
Aaron Chu <aaronchu357@gmail.com>
2019-03-13 17:42:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function dwarfRollCall(dwarves) {
+  var called = ""
+  for (i = 0; i <= dwarves.length; i++){
+  called += (i+1 + "." + dwarves[0] + " ")
+  }
+  return called
 }
 
 function summonCaptainPlanet(planeteerCalls){
