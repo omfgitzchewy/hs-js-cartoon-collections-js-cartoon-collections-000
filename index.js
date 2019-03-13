4950ec61fd60a141d84ba85136aa3ecded45f826
Aaron Chu <aaronchu357@gmail.com>
2019-03-13 17:48:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function dwarfRollCall(dwarves) {
   var str = ""
   for (var i = 0; i < dwarves.length; i++){
-    called += (i+1 + ". " + dwarves[i] + " ")
+    str += (i+1 + ". " + dwarves[i] + " ")
   }
   return str
 }
